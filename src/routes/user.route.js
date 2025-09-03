455095d1c5581c34b158b2b4c1d6a364de113ecc
Sunny Verma <161021054+sunnyV0207@users.noreply.github.com>
2025-09-03 10:34:21 +0530

    Add welcome route to user router

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,6 +3,9 @@ import { validateEmail } from "../middlewares/validateEmail.middleware.js";
 import {registerUser,googleAuth,loginUser,verifyEmail} from '../controllers/user.controller.js'
 
 const router = Router();
+router.route('/home').get((req,res)=>{
+    res.send("Welcome");
+})
 router.route('/register').post(validateEmail ,registerUser);
 router.route('/google').post(googleAuth);
 router.route('/login').post(loginUser);
