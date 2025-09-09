35939399a6c98b8b6874995af0d5e9564e1b7966
Sunny Verma <161021054+sunnyV0207@users.noreply.github.com>
2025-09-09 17:16:23 +0530

    Add Twilio integration for OTP verification and update user routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { validateEmail } from "../middlewares/validateEmail.middleware.js";
-import {registerUser,googleAuth,loginUser,verifyEmail} from '../controllers/user.controller.js'
+import {registerUser,googleAuth,loginUser,verifyUser} from '../controllers/user.controller.js'
 
 const router = Router();
 router.route('/home').get((req,res)=>{
@@ -9,6 +9,6 @@ router.route('/home').get((req,res)=>{
 router.route('/register').post(validateEmail ,registerUser);
 router.route('/google').post(googleAuth);
 router.route('/login').post(loginUser);
-router.route('/verify-email').get(verifyEmail);
+router.route('/verify-user').post(verifyUser);
 
 export default router;
