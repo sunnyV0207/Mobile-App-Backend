bb691ed61542cd489b766deb7e6ac445a580af90
Sunny Verma <161021054+sunnyV0207@users.noreply.github.com>
2025-09-15 15:42:36 +0530

    Update user.route.js

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { validateEmail } from "../middlewares/validateEmail.middleware.js";
-import {registerUser,googleAuth,loginUser,verifyEmail} from '../controllers/user.controller.js'
+import {verifyJwt} from "..//middlewares/auth.middleware.js";
+import {registerUser,googleAuth,loginUser,verifyEmail,checkAuth} from '../controllers/user.controller.js'
 
 const router = Router();
 router.route('/home').get((req,res)=>{
@@ -10,5 +11,7 @@ router.route('/register').post(validateEmail ,registerUser);
 router.route('/google').post(googleAuth);
 router.route('/login').post(loginUser);
 router.route('/verify-user').get(verifyEmail);
+router.route('/check-auth').post(verifyJwt,checkAuth);
+
 
 export default router;
